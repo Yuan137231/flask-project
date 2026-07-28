5eaa82ea261f2fae6077cf69301edeb8e53095c0
ysx <2417372948@qq.com>
2026-07-28 23:39:37 +0800

    页面美化

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 from config import Config
@@ -16,6 +17,7 @@ print(app.config['SECRET_KEY'])
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 mail=Mail(app)
+bootstrap=Bootstrap(app)
 
 # 延迟导入，解决循环导入报错
 
